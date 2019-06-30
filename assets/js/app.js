1f4aa10a1ad8dd75850354b2fe11dedc89bd9a0d
Karim <abdounikarim@gmail.com>
2019-06-30 12:30:22 +0200

    Modules: require() & import()

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,9 +6,11 @@
  */
 
 // any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
+import('../css/app.css');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // const $ = require('jquery');
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js!!!');
+const getNiceMessage = require('./get_nice_message');
+
+console.log(getNiceMessage(5));
