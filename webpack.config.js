e098e24f1d203c10acc8946abd3bb983a42874b4
Karim <abdounikarim@gmail.com>
2019-07-01 19:57:18 +0200

    Copying Files

--- webpack.config.js
+++ webpack.config.js
@@ -57,6 +57,11 @@ Encore
     // enables Sass/SCSS support
     .enableSassLoader()
 
+    .copyFiles({
+        from: './assets/images',
+        to: 'images/[path][name].[hash:8].[ext]'
+    })
+
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
 
